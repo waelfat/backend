14890b00503fc57ebc4987ea42788d79469625f2
wael fathy <waelfathy2007@gmail.com>
2021-12-30 23:02:26 +0200

    listen

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-import f from 'express';
+import express from 'express';
+import cors from    'cors'
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+require('dotenv').config()
+
+
+
+
+const app=express()
+app.use(cors()).use(morgan()).use(helmet())
+app.get('/',(req,res,next)=>{
+    res.json('ehll')
+})
+app.listen(process.env.PORT,()=>{
+    console.log('LISTEN');
+})
